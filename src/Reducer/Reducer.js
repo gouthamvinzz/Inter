9731f900e6392c90e4e4f6112771c22eda1bb326
gouthamvinzz <gouthamsv28@gmail.com>
2020-02-24 18:56:20 +0530

    updated reducer

--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -1,16 +1,21 @@
 import { ADD_ONE,MINUS_ONE } from "./../Action/Action";
+import axios from 'axios';
 const initialState ={
-    number:[0]
+    number:[]
 }
 const Reducer = (state = initialState ,action)=>{
     console.log('aaa',state,action)
 switch(action.type)
 {
     case 'ADD_ONE':
-        {
+        { axios.get(`https://api.openweathermap.org/data/2.5/weather?q=`+action.payload+`&appid=af8604b40a59019d724776f00f179133`)
+        .then(res=>{
+            console.log('res',res)
             return{
-                number: [...state.number,action.payload]
+                number: state.number[0]=res.data.main
             };
+        })
+            
         }
     case 'MINUS_ONE':
         {
